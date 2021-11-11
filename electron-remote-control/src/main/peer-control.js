3c257fee525082381b90a17ea5cdc70c4bb142c5
浅沫漓殇 <924181956@qq.com>
2021-11-11 22:25:12 +0800

    2021-11-11 22:25

--- electron-remote-control/src/main/peer-control.js
+++ electron-remote-control/src/main/peer-control.js
@@ -34,4 +34,43 @@ peer.on('robot', (type, data) => {
     ipcRenderer.send('robot', type, data)
 })
 
+const pc = new window.RTCPeerConnection({})
+pc.onicecandidate = function (e) {
+    console.log(e.candidate)
+}
+
+let candidates = []
+async function addIceCandidate(candidate) {
+    if (candidate) {
+        candidates.push(candidate)
+    }
+    if (pc.remoteDescription && pc.remoteDescription.type) {
+        for (let i = 0; i < candidates.length; i++) {
+            await pc.addIceCandidate(new RTCIceCandidate(candidate))
+        }
+        candidates = []
+    }
+}
+window.addIceCandidate = addIceCandidate
+
+async function createOffer() {
+    const offer = pc.createOffer({
+        offerToReceiveAudio: false,
+        offerToReceiveVideo: true
+    })
+    await pc.setLocalDescription(offer)
+    return pc.localDescription
+}
+
+
+createOffer()
+
+async function setRomote(answer) {
+    await pc.setRemoteDescription(answer)
+}
+
+window.setRomote = setRomote
+pc.onaddstream = function (e) {
+    peer.emit('add-stream', e.stream)
+}
 module.exports = { peer }
